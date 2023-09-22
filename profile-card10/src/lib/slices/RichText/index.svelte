3821e321064bf6414f235f1ee5fa6ad21222bd08
Tobias Vrolijk <vrolijktobias@gmail.com>
2023-09-22 10:42:38 +0200

    push

--- profile-card10/src/lib/slices/RichText/index.svelte
+++ profile-card10/src/lib/slices/RichText/index.svelte
@@ -11,14 +11,23 @@
 		<div class="avatar">
 			<PrismicImage field={slice.primary.avatar} />
 		</div>
+
 		<div class="name">
 			<h1>Tobias Vrolijk</h1>
 		</div>
-		<div class="description">
+
+		<div class="github-link">
+		   <a href="https://github.com/TobiasVr23" target="_blank">
 			<PrismicImage field={slice.primary.githubimage} />
-			<PrismicImage field={slice.primary.locationname} />
+		   </a>
 		</div>
-		
+
+		<div class="description1">
+			<PrismicImage field={slice.primary.locationname} /><h2>Amsterdam</h2>
+		</div>
+		<div class="description2">
+			<h2>📅 31-03-2003</h2>
+		</div>
 
 	</div>
 </body>
@@ -28,15 +37,22 @@
 		height: 100vh;
 		width: 25em;
 		background: rgb(85,139,245);
-		background: linear-gradient(180deg, rgba(85,139,245,1) 35%, rgba(204,221,255,1) 100%);
+		background: linear-gradient(180deg, rgba(85,139,245,1) 60%, #ffd09c 100%);
 		border-radius: 15px;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
 	}
-	.name{
-		margin-top: 3em;
+
+	.description1{
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-evenly;
 	}
+
+
+
 
 </style>
